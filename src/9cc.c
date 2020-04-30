44093aaa7ed9984e503cd67608e70a87a39661c1
thedoritos <thedoritos@gmail.com>
2020-04-30 22:21:19 +0900

    :sparkles: Parse +&- by tree

--- src/9cc.c
+++ src/9cc.c
@@ -185,24 +185,16 @@ int main(int argc, char **argv) {
   }
 
   user_input = argv[1];
-  token = tokenize(argv[1]);
+  token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  printf("    mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("    add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("    sub rax, %d\n", expect_number());
-  }
-
+  printf("    pop rax\n");
   printf("    ret\n");
   return 0;
 }
