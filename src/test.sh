8b75eacc8ffd57ad684f6902819664ea37d8dc80
thedoritos <thedoritos@gmail.com>
2020-05-09 23:38:40 +0900

    :white_check_mark: Test return statement

--- src/test.sh
+++ src/test.sh
@@ -52,4 +52,7 @@ try 2 "xyz = 2; xyz;"
 try 10 "abc = 1; efg = 2 * 3; abc + efg + 3;"
 try 2 "ant = 1; apple = 2; ant * apple;"
 
+try 1 "a = 1; return a; 99;"
+try 2 "b = 1; return b * 2; return 99;"
+
 echo OK
